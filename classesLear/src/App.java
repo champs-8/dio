9249551b41a407510c89ecc4a1ba47a47984bb2c
champs-8 <102865155+champs-8@users.noreply.github.com>
2025-06-24 19:04:36 -0300

    feat: primeiro passos na orientação a objetos

---
+++ classesLear/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        var male = new Person("John"); //instancia de masculino
+
+        var female = new Person("Jane"); //instancia de femenino
+
+        System.out.printf("Male name: %s, age: %d\n", male.getName(),male.getAge());
+        System.out.printf("Female name: %s, age: %d\n", female.getName(),female.getAge());
+    }
+}
