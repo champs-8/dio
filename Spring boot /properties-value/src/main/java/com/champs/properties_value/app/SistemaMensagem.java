5873ddb3d990db6495f0215e0a105dc2e78bca07
champs-8 <102865155+champs-8@users.noreply.github.com>
2025-07-14 23:01:21 -0300

    aprendendo sobre properties value

---
+++ Spring boot /properties-value/src/main/java/com/champs/properties_value/app/SistemaMensagem.java
@@ -0,0 +1,28 @@
+package com.champs.properties_value.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+
+    //atributos
+    //valores vindos do arquivo application.properties
+    @Value("${name:Noreaply-Irineu}") // valor padrão caso não seja definido
+    private String name;
+    @Value("${email}")
+    private String email;
+    @Value("${phones}")
+    private List<Integer> phones;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.printf("Mensagem enviada para %s%nEmail: %s%nTelefone: %s", name, email, phones);
+        System.out.println("\nSeu cadastro foi realizado com sucesso!");
+    }
+}
