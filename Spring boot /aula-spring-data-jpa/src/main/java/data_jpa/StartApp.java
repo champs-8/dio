498edc57563de548c1f065d966bec55d76a08af7
champs-8 <102865155+champs-8@users.noreply.github.com>
2025-07-15 23:16:02 -0300

    finalizando introdução à banco de dados postgreSQL

--- Spring boot /aula-spring-data-jpa/src/main/java/data_jpa/StartApp.java
+++ Spring boot /aula-spring-data-jpa/src/main/java/data_jpa/StartApp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
@@ -13,10 +15,17 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<User> user = repository.filtrarPorNome("Irineu");
+        for (User u: user) {
+            System.out.println(u);
+        }
+
+    }
+    public void insertUser() {
         User user = new User();
-        user.setName("CHAMPS");
-        user.setUsername("champs_8");
-        user.setPassword("123456");
+        user.setName("Irineu pika fina");
+        user.setUsername("OtalDoIrineu");
+        user.setPassword("eu");
 
         repository.save(user); // Save the user to the database
 
--- Spring boot /aula-spring-data-jpa/src/main/java/data_jpa/repository/UserRepository.java
+++ Spring boot /aula-spring-data-jpa/src/main/java/data_jpa/repository/UserRepository.java
@@ -2,9 +2,23 @@ package data_jpa.repository;
 
 import data_jpa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     // This interface will automatically provide CRUD operations for User entities
     // No additional methods are needed unless custom queries are required
+
+    // Example of a custom query method
+    List<User> findByNameContaining(String name);
+
+    // Example of a custom query method
+    User findByUsername(String username);
+
+    // Example of a custom query override
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(@Param("name")String name);
 }
